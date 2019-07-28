3406d3faa47cb1ae005452d3900719619367377b
ParkSungHoon <shcjswo8921@gmail.com>
2019-07-28 21:52:59 +0900

    create mvp pattern view interface

---
+++ app/src/main/java/com/girin/reminderapplication/view/IMainView.java
@@ -0,0 +1,9 @@
+package com.girin.reminderapplication.view;
+
+import com.girin.reminderapplication.model.Reminder;
+
+import java.util.List;
+
+public interface IMainView {
+    void setReminder(List<Reminder> reminder);
+}
