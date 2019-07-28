97ce0f93465864d851546a2baccf7722c8d7976e
ParkSungHoon <shcjswo8921@gmail.com>
2019-07-28 21:53:23 +0900

    implements IMainView

--- app/src/main/java/com/girin/reminderapplication/MainActivity.java
+++ app/src/main/java/com/girin/reminderapplication/MainActivity.java
@@ -8,9 +8,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.girin.reminderapplication.adapter.ReminderAdapter;
+import com.girin.reminderapplication.model.Reminder;
+import com.girin.reminderapplication.view.IMainView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements IMainView {
 
     private TextView mainContent;
     private RecyclerView reminderList;
@@ -18,15 +22,24 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayoutManager linearLayoutManager;
     private FloatingActionButton reminderAdd;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
         mainContent = findViewById(R.id.main_content);
         reminderList = findViewById(R.id.reminder_list);
         reminderAdapter = new ReminderAdapter();
         linearLayoutManager = new LinearLayoutManager(this);
         reminderAdd = findViewById(R.id.reminder_add);
+
+    }
+
+    @Override
+    public void setReminder(List<Reminder> reminder) {
+
     }
 }
