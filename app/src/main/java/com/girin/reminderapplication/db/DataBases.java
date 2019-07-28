7318677d08519e7401e7ec38309ab3266e6c2130
ParkSungHoon <shcjswo8921@gmail.com>
2019-07-28 21:51:26 +0900

    create db command

---
+++ app/src/main/java/com/girin/reminderapplication/db/DataBases.java
@@ -0,0 +1,23 @@
+package com.girin.reminderapplication.db;
+
+import android.provider.BaseColumns;
+
+public final class DataBases {
+
+    public static final class
+    CreateDB implements BaseColumns {
+        public static final String TITLE = "title";
+        public static final String CONTENT = "content";
+        public static final String DATE = "date";
+        public static final String ALRAM_CHECK = "alram_check";
+        public static final String _TABLENAME0 = "reminder";
+        public static final String _CREATE0 = "create table if not exists " + _TABLENAME0
+                +"("
+                +_ID+" integer primary key autoincrement, "
+                +TITLE+" text not null , "
+                +CONTENT+" text not null , "
+                +DATE+" text not null , "
+                +ALRAM_CHECK+" integer not null );";
+
+    }
+}
