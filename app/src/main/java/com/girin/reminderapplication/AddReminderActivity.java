71a20557da3379050c8930a4639ece2b89861640
sunghoon <ndspsh@gmail.com>
2019-08-13 17:47:49 +0900

    add millis_second field & alarm broad cast

--- app/src/main/java/com/girin/reminderapplication/AddReminderActivity.java
+++ app/src/main/java/com/girin/reminderapplication/AddReminderActivity.java
@@ -3,6 +3,7 @@ package com.girin.reminderapplication;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +12,15 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.girin.reminderapplication.db.DataBases;
+import com.girin.reminderapplication.db.DbOpenHelper;
 import com.girin.reminderapplication.model.Reminder;
 import com.girin.reminderapplication.presenter.AddReminderPresenter;
+import com.girin.reminderapplication.util.Util;
 import com.girin.reminderapplication.view.IAddReminderView;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -30,6 +35,7 @@ public class AddReminderActivity extends AppCompatActivity implements IAddRemind
     private DatePickerDialog datePickerDialog;
     private int year, month, date, hour, minute;
     private TimePickerDialog timePickerDialog;
+    private Reminder reminder;
 
     private AddReminderPresenter addReminderPresenter;
 
@@ -77,38 +83,30 @@ public class AddReminderActivity extends AppCompatActivity implements IAddRemind
             }
         }, year, month, date);
         datePickerDialog.getDatePicker().setMinDate(new Date().getTime());
-        tvPickDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                datePickerDialog.show();
-            }
+        tvPickDate.setOnClickListener((View view) -> {
+            datePickerDialog.show();
         });
 
-        alarmCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                alarmCheck.setSelected(!alarmCheck.isSelected());
-            }
+        alarmCheck.setOnClickListener((View view) -> {
+            alarmCheck.setSelected(!alarmCheck.isSelected());
         });
 
-        btnAddReminder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (reminderTitle != null && reminderContent != null && alarmCheck != null && tvPickDate != null) {
-                    if (!reminderTitle.getText().toString().isEmpty() && !tvPickDate.getText().toString().equals("날짜 선택")) {
-                        Reminder reminder = new Reminder();
-                        reminder.setTitle(reminderTitle.getText().toString());
-                        reminder.setContent("");
-                        if (!reminderContent.getText().toString().equals(getResources().getString(R.string.add_reminder_content)))
-                            reminder.setContent(reminderContent.getText().toString());
-                        reminder.setTime(tvPickDate.getText().toString());
-                        reminder.setAlertCheck(0);
-                        if (alarmCheck.isSelected())
-                            reminder.setAlertCheck(1);
-                        addReminderPresenter.addReminderToDb(reminder);
-                    } else
-                        Snackbar.make(findViewById(R.id.add_reminder_layout), getResources().getString(R.string.add_reminder_error), 2000).show();
-                }
+        btnAddReminder.setOnClickListener((View view) -> {
+            if (reminderTitle != null && reminderContent != null && alarmCheck != null && tvPickDate != null) {
+                if (!reminderTitle.getText().toString().isEmpty() && !tvPickDate.getText().toString().equals("날짜 선택")) {
+                    reminder = new Reminder();
+                    reminder.setTitle(reminderTitle.getText().toString());
+                    reminder.setContent("");
+                    if (!reminderContent.getText().toString().equals(getResources().getString(R.string.add_reminder_content)))
+                        reminder.setContent(reminderContent.getText().toString());
+                    reminder.setTime(tvPickDate.getText().toString());
+                    reminder.setAlertCheck(0);
+                    if (alarmCheck.isSelected())
+                        reminder.setAlertCheck(1);
+                    reminder.setMilli_second(Util.stringToDate(reminder.getTime()).getTime());
+                    addReminderPresenter.addReminderToDb(reminder);
+                } else
+                    Snackbar.make(findViewById(R.id.add_reminder_layout), getResources().getString(R.string.add_reminder_error), 2000).show();
             }
         });
     }
@@ -128,6 +126,20 @@ public class AddReminderActivity extends AppCompatActivity implements IAddRemind
         //Intent return success
         intent.putExtra("result", result);
         setResult(RESULT_OK, intent);
+        if (result > 0) {
+            DbOpenHelper dbOpenHelper = new DbOpenHelper(AddReminderActivity.this);
+            dbOpenHelper.open();
+            Cursor cursor = dbOpenHelper.selectLastColumn();
+            if (!cursor.moveToFirst()) {
+                cursor.close();
+                dbOpenHelper.close();
+                finish();
+            }
+            reminder.set_id(cursor.getInt(cursor.getColumnIndex(DataBases.CreateDB._ID)));
+            Util.addAlarm(reminder.get_id(), reminder.getTime(), AddReminderActivity.this);
+            cursor.close();
+            dbOpenHelper.close();
+        }
         finish();
 
     }
