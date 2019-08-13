b8fe7f6c0ff86e1e8e09e42c7996e43cc98c939b
sunghoon <ndspsh@gmail.com>
2019-08-13 17:43:53 +0900

    add milli_second field

--- app/src/main/java/com/girin/reminderapplication/db/DataBases.java
+++ app/src/main/java/com/girin/reminderapplication/db/DataBases.java
@@ -10,6 +10,7 @@ public final class DataBases {
         public static final String CONTENT = "content";
         public static final String DATE = "date";
         public static final String ALRAM_CHECK = "alram_check";
+        public static final String MIllI_SECOND = "milli_second";
         public static final String _TABLENAME0 = "reminder";
         public static final String _CREATE0 = "create table if not exists " + _TABLENAME0
                 +"("
@@ -17,7 +18,7 @@ public final class DataBases {
                 +TITLE+" text not null , "
                 +CONTENT+" text not null , "
                 +DATE+" text not null , "
-                +ALRAM_CHECK+" integer not null );";
-
+                +ALRAM_CHECK+" integer not null, "
+                +MIllI_SECOND+" integer not null);";
     }
 }
